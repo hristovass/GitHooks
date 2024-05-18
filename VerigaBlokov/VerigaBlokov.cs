605a8edb0ddd6fa4f820b0d789b5179b8ad87e1b
Lazar2201 <lazar.maksimovic@student.um.si>
2024-05-18 22:11:19 +0200

    Test pre-commit hook

--- VerigaBlokov/VerigaBlokov.cs
+++ VerigaBlokov/VerigaBlokov.cs
@@ -1,17 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
 using System.Net;
 using System.Net.Sockets;
-using System.Threading;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
 using Newtonsoft.Json;
+
+
 
 namespace VerigaBlokov
 {
